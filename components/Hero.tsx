360298dff56092cfde95de0c98a80dc0043a21e6
Francisco Costa <francisco100eg@gmail.com>
2023-06-27 11:56:57 -0300

    Feat: Add Tailwind CSS config and font files

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react'
+
+
+const Hero = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "plus-jakarta-sans": ["Plus Jakarta Sans", "sans-serif"],
+      },
+      fontWeight: {
+        bold: "bold",
+        "bold-italic": "bold italic",
+        "extra-bold": 800,
+        "extra-bold-italic": "800 italic",
+        "extra-light": 200,
+        "extra-light-italic": "200 italic",
+        light: 300,
+        "light-italic": "300 italic",
+        medium: 500,
+        "medium-italic": "500 italic",
+        regular: "normal",
+        "semi-bold": 600,
+        "semi-bold-italic": "600 italic",
+      },
+      colors: {
+        bgWhite: "#ffffff",
+        bglBlue: "#f0f8ff",
+        txtBlack: "#121212",
+        txtBlue: "#0083ff",
+        txtGray: "#93a5b8",
+
+
       },
     },
   },
   plugins: [],
-}
+};
