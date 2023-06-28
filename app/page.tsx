678c789b33371795df89094b4bb71d5d34ebf42b
Francisco Costa <francisco100eg@gmail.com>
2023-06-28 19:49:43 -0300

    Fix: Deleted CostumSelect and added filter and order functionality to filters

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { mockDataItem } from "../types/index"
 import React, { useState, useEffect } from 'react';
-import { Hero, Reviews, CustomFilter, CustomSelect, CardItem } from '@/components';
+import { Hero, Reviews, CustomFilter, CardItem } from '@/components';
 
 import datamock from '../public/cards.json';
 
@@ -11,16 +11,43 @@ export default function Home() {
   useEffect(() => {
 
     setActiveData(datamock as mockDataItem[])
+    setFilteredData(datamock as mockDataItem[])
   }, []);
 
   const [activeFilter, setActiveFilter] = useState('');
   const [data, setActiveData] = useState<mockDataItem[]>([]);
-  
-  const handleClick = (title : string) => {
+  const [filteredData, setFilteredData] = useState<mockDataItem[]>([]);
+
+  const handleClick = (title: string) => {
     setActiveFilter(title);
+    if (title === 'Agências') {
+      setFilteredData(data.filter((item) => item.tags.includes('Agências')));
+    }
+    if (title === 'Chatbot') {
+      setFilteredData(data.filter((item) => item.tags.includes('Chatbot')));
+    }
+    if (title === 'Marketing Digital') {
+      setFilteredData(data.filter((item) => item.tags.includes('Marketing Digital')));
+    }
+    if (title === 'Geração de Leads') {
+      setFilteredData(data.filter((item) => item.tags.includes('Geração de Leads')));
+    }
+    if (title === 'Mídia Paga') {
+      setFilteredData(data.filter((item) => item.tags.includes('Mídia Paga')));
+    }
+    if (title === '') {
+      setFilteredData(data);
+    }
+    if (title === 'recent') {
+      setFilteredData(filteredData.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()));
+    console.log(filteredData)
+    }
+    if (title === 'oldest') {
+      setFilteredData(filteredData.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()));
+    console.log(filteredData)
+    }
   };
 
-  console.log(data);
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -35,13 +62,20 @@ export default function Home() {
               <CustomFilter title="Mídia Paga" handleClick={() => handleClick('Mídia Paga')} isActive={activeFilter === 'Mídia Paga'} />
             </div>
             <div className='Content__order'>
-              <CustomSelect />
+              <div className='Filter__select'>
+                <label htmlFor="orderby" className='font-bold text-color-black mr-4'>Ordenar por:</label>
+                <select name="orderby" className='custom-select-filter' id="orderby" placeholder='Data de Publicação' onChange={(event) => handleClick(event.target.value)}>
+                  <option value="recent" >Data de publicação</option>
+                  <option value="recent" >Mais Recente</option>
+                  <option value="oldest" >Mais Antigo</option>
+                </select>
+              </div>
             </div>
           </div>
-          <div className='line__filter'/>
+          <div className='line__filter' />
           <div className='Cards__container'>
             {
-              data?.map((card)=> <CardItem card={card} />)
+              filteredData?.map((card) => <CardItem card={card} />)
             }
           </div>
         </div>
--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,8 @@ import Hero from './Hero';
 import Footer from './Footer';
 import Reviews from './Reviews';
 import CustomFilter from './CustomFilter';
-import CustomSelect from './CustomSelect';
 import CardItem from './CardItem';
 import CardDetails from './CardDetails';
 import CustomDowloadBtn from './CustomDowloadBtn';
 
-export { Hero, Footer, Reviews, CustomFilter, CustomSelect, CardItem, CardDetails, CustomDowloadBtn };
+export { Hero, Footer, Reviews, CustomFilter, CardItem, CardDetails, CustomDowloadBtn };
